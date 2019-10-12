c10c0139ac31f020356f6e08f1ede0e7a4ec7b62
KuozDavid <david.palta.anes1989@gmail.com>
2019-10-12 16:51:30 -0300

    Instegración de nuevos repository.

---
+++ src/main/kotlin/com/example/pruebadegradleparatesis/Repository/ImagenesRepository.kt
@@ -0,0 +1,7 @@
+package com.example.pruebadegradleparatesis.Repository
+
+import com.example.pruebadegradleparatesis.Model.Imagenes
+import org.springframework.data.jpa.repository.JpaRepository
+
+interface ImagenesRepository:JpaRepository<Imagenes,Int> {
+}
